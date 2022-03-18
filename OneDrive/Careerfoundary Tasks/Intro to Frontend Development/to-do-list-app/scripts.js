67187bd897155e5d9db3ff3aef332829ef4a4277
sabertooth54 <mako95@hotmail.co.uk>
2022-03-18 15:06:38 +0100

    changes made to html

--- OneDrive/Careerfoundary Tasks/Intro to Frontend Development/to-do-list-app/scripts.js
+++ OneDrive/Careerfoundary Tasks/Intro to Frontend Development/to-do-list-app/scripts.js
@@ -1,68 +1,3 @@
-/*
-function newItem(){
-
-//javascript
-//1. Adding a new item to the list of items: 
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.appendChild(text);
-
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
-   }
-
-
-
- //2. Crossing out an item from the list of items:
-   function crossOut() {
- 		li.classList.toggle("strike");
- 	}
-
- 	li.addEventListener("dblclick",crossOut);
-//from her is mine
- 
-
-  
- //3(i). Adding the delete button "X": 
-   let crossOutButton = document.createElement("crossOutButton");
- 	crossOutButton.appendChild(document.createTextNode("X"));
- 	li.appendChild(crossOutButton);
-
- 	crossOutButton.addEventListener("click", deleteListItem);
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem(){
- 		li.classList.add("delete")
- 	}
- // 4. Reordering the items: 
-   $('#list').sortable();
-
-}
-
- */
-
-
-
-
-
-
-
-// If you get stuck, you can look below for the jQuery code. However, try yourself to convert the vanilla JS code provided to jQuery first.
-
-
-
-
-
-
-
-
-  
-// jQuery Code
-//1. Adding a new item to the list:
-
 function newItem(){
 
     let li = $('<li></li>');
@@ -95,5 +30,3 @@ function newItem(){
   //*/
   
     }
-  
-  
